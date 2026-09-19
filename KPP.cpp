01c6e5da8477ca1d839f4439ffe1e51a6d97c46d
kenziattabany-png <kenzi.attabany@gmail.com>
2026-09-19 22:22:34 +0700

    Refactor maze solving algorithm

--- KPP.cpp
+++ KPP.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
+const int rows = 7;
+const int cols = 7;
+const int MAX = 100;
 
-const int ROWS = 7;
-const int COLS = 7;
-const int MAX_STEPS = 100;
 
-char maze[ROWS][COLS] = {
+char Grid[rows][cols] = {
     {'#', '#', '#', '#', '#', '#', '#'},
     {'#', 'S', '#', '.', '.', '.', '#'},
     {'#', '.', '#', '.', '#', 'G', '#'},
@@ -16,154 +17,252 @@ char maze[ROWS][COLS] = {
     {'#', '#', '#', '#', '#', '#', '#'}
 };
 
-bool visited[ROWS][COLS][2] = {};
+bool visited[rows][cols] = {};
+char path[MAX];
+int pathLength = 0;
 
-char path[MAX_STEPS];
-int pathlength = 0;
 
-void printMaze()
-{
-
-    for (int row = 0; row < ROWS; row++)
-    {
-        for (int col = 0; col < COLS; col++)
-        {
-            cout << maze[row][col] << ' ';
+void Viz(){
+    for (int row = 0; row < rows; row++){
+        for (int col = 0; col < cols; col++){
+            cout << Grid[row][col] << ' ';
         }
-
         cout << endl;
     }
 }
 
-bool solveMaze(int row, int col, bool hasF){
-    if (row < 0 || row >= ROWS || col < 0 || col >= COLS || maze[row][col] == '#' || maze[row][col] == 'X' || visited[row][col][hasF])
-    {
+bool solveMaze(int row, int col, int targetRow, int targetCol, bool hasFlag){
+    if (row < 0 || row >= rows || col < 0 || col >= cols){
         return false;
     }
 
-    if (maze[row][col] == 'F')
-    {
-        hasF = true;
-    }
-
-    int state = hasF ? 1 : 0;
-    if (visited[row][col][state])
-    {
+    if (Grid[row][col] == '#' || Grid[row][col] == 'X'){
         return false;
     }
-    visited[row][col][state] = true;
 
-    if (maze[row][col] == 'G')
-    {
-        if (hasF)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    // Cannot enter G before getting the flag
+    if (Grid[row][col] == 'G' && !hasFlag){
+        return false;
     }
 
-    if (solveMaze(row - 1, col, hasF)){
-        path[pathlength] = 'U';
-        pathlength++;
+    if (visited[row][col]){
+        return false;
+    }
+
+    visited[row][col] = true;
+
+    if (row == targetRow && col == targetCol){
         return true;
     }
 
-    if (solveMaze(row + 1, col, hasF)){
-        path[pathlength] = 'D';
-        pathlength++;
+    if (solveMaze(row - 1, col, targetRow, targetCol, hasFlag)){
+        path[pathLength] = 'U';
+        pathLength++;
         return true;
     }
 
-    if (solveMaze(row, col - 1, hasF)){
-        path[pathlength] = 'L';
-        pathlength++;
+    if (solveMaze(row + 1, col, targetRow, targetCol, hasFlag)){
+        path[pathLength] = 'D';
+        pathLength++;
         return true;
     }
 
-    if (solveMaze(row, col + 1, hasF)){
-        path[pathlength] = 'R';
-        pathlength++;
+    if (solveMaze(row, col - 1, targetRow, targetCol, hasFlag)){
+        path[pathLength] = 'L';
+        pathLength++;
+        return true;
+    }
+
+    if (solveMaze(row, col + 1, targetRow, targetCol, hasFlag)){
+        path[pathLength] = 'R';
+        pathLength++;
         return true;
     }
 
     return false;
 }
 
-void markPath (int startRow, int startCol){
-
-    int row = startRow;
-    int col = startCol;
-
-    for (int i = pathlength -1; i >= 0; i--){
-        int nextRow = row;
-        int nextCol = col;
-
-        if (path[i] == 'U'){
-            nextRow--;
-        }
-        else if (path[i] == 'D'){
-            nextRow++;
-        }
-        else if (path[i] == 'L'){
-            nextCol--;
-        }
-        else if (path[i] == 'R'){
-            nextCol++;
-        }
-
-        if (maze[nextRow][nextCol] == '.'){
-            maze[nextRow][nextCol] = 'R';
-        }
-
-        row = nextRow;
-        col = nextCol;
-    }
-}
 
 void printPath(){
-
-    cout << "\nPath:\n";
-
-    for (int i=pathlength - 1; i >= 0; i--){
-        cout << path[i] << ' ';
+    for (int pathIndex = pathLength - 1;pathIndex >= 0;pathIndex--){
+        cout << path[pathIndex] << ' ';
     }
     cout << endl;
 }
 
-int main(){
 
-    cout << "Maze: " << endl;
+void resetSearch(){
+    memset(visited,false,sizeof(visited));
+    pathLength = 0;
+}
+
+
+void markPath(int startRow,int startCol){
+    int currentRow = startRow;
+    int currentCol = startCol;
+
+    for (int pathIndex = pathLength - 1;pathIndex >= 0;pathIndex--){
+        int nextRow = currentRow;
+        int nextCol = currentCol;
+
+        if (path[pathIndex] == 'U'){
+            nextRow--;
+        }
+        else if (path[pathIndex] == 'D'){
+            nextRow++;
+        }
+        else if (path[pathIndex] == 'L'){
+            nextCol--;
+        }
+        else if (path[pathIndex] == 'R'){
+            nextCol++;
+        }
+
+        if (nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols &&Grid[nextRow][nextCol] == '.'){
+            Grid[nextRow][nextCol] = 'R';
+        }
+        currentRow = nextRow;
+        currentCol = nextCol;
+    }
+}
+
+
+int main()
+{
+
+    cout << "======================================" << endl;
+    cout << "          MAZE ROBOT PROGRAM          " << endl;
+    cout << "======================================" << endl;
+
+    cout << endl;
+
+
+    cout << "Robot: Voyager" << endl;
+
+    cout << endl;
+
 
     int startRow = -1;
     int startCol = -1;
 
-    for (int row = 0; row < ROWS; row++){
-        for (int col = 0; col < COLS; col++){
-            if (maze[row][col] == 'S'){
-                startRow = row;
-                startCol = col;
+    int flagRow = -1;
+    int flagCol = -1;
+
+    int goalRow = -1;
+    int goalCol = -1;
+
+
+    for (int rowIndex = 0; rowIndex < rows; rowIndex++){
+        for (int colIndex = 0; colIndex < cols; colIndex++){
+            if (Grid[rowIndex][colIndex] == 'S'){
+                startRow = rowIndex;
+                startCol = colIndex;
+            }
+            else if (Grid[rowIndex][colIndex] == 'F'){
+                flagRow = rowIndex;
+                flagCol = colIndex;
+            }
+            else if (Grid[rowIndex][colIndex] == 'G'){
+                goalRow = rowIndex;
+                goalCol = colIndex;
             }
         }
     }
 
-    printMaze();
 
-    bool solved = solveMaze(startRow, startCol, false);
 
-    if (solved){
-        cout << "\nMaze Solved!\n";
-        printPath();
-        markPath(startRow, startCol);
-        cout << "\nSolution:\n";
-        printMaze();
-        cout << "\nTotal Steps: " << pathlength << endl;
+    cout << "Initial Maze:" << endl;
+
+    Viz();
+
+    cout << endl;
+
+
+   bool flagFound = solveMaze(startRow, startCol, flagRow, flagCol,false);
+
+
+    if (!flagFound){
+        cout << endl;
+        cout << "No path to FLAG found." << endl;
+        return 0;
     }
 
-    else {   
-        cout << "\nNo solution found.\n";
+    int stepsToFlag = pathLength;
+
+    cout << endl;
+
+    cout << "Path S -> F:" << endl;
+
+    printPath();
+
+    cout << endl;
+
+    cout << "FLAG CAPTURED!" << endl;
+
+    markPath(startRow, startCol);
+    Viz();
+    resetSearch();
+
+
+    cout << endl;
+
+
+bool goalFound = solveMaze(flagRow, flagCol, goalRow, goalCol, true);
+
+    if (!goalFound){
+        cout << endl;
+        cout << "No path to GOAL found." << endl;
+        return 0;
     }
+
+
+
+    int stepsToGoal = pathLength;
+
+    cout << endl;
+
+    cout << "Path F -> G:" << endl;
+
+    printPath();
+
+
+    cout << endl;
+
+    cout << "MISSION COMPLETE!" << endl;
+
+
+    int totalSteps =stepsToFlag + stepsToGoal;
+
+
+    cout << endl;
+
+    cout << "Steps S -> F: "
+         << stepsToFlag
+         << endl;
+
+    cout << "Steps F -> G: "
+         << stepsToGoal
+         << endl;
+
+    cout << "Total Steps: "
+         << totalSteps
+         << endl;
+
+    markPath(flagRow,flagCol);
+
+
+    cout << endl;
+
+    cout << "Final Map:" << endl;
+
+    Viz();
+
+
+    cout << endl;
+    cout << "======================================" << endl;
+    cout << "              FINISHED                " << endl;
+    cout << "======================================" << endl;
+
+
     return 0;
 }
